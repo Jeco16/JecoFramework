dd6b7481aa63ab5786d73ec2dfff234e3d4dc75a
Jacopo Enrico Marinaccio <jeco.16.11.2000@gmail.com>
2026-08-17 22:36:54 +0200

    Test to run smoke in CI\CD

--- tests/e2e/suiteTemplate/Test001_Login.spec.js
+++ tests/e2e/suiteTemplate/Test001_Login.spec.js
@@ -7,7 +7,7 @@ import { suite } from './suite.config.js';
 import { test } from '../../fixtures/fixtures.js';
 
 test.describe(suite.name, () => {
-  test('Test template 001 - login e logout with fixture', async ({ basePage, templatePage }) => {
+  test('[@smoke] Test template 001 - login e logout with fixture', async ({ basePage, templatePage }) => {
     await basePage.open(suite.baseURL, /Swag Labs/);
     await templatePage.login(suite.env.credentials.USER_1, suite.env.credentials.PASSWORD);
     await templatePage.logout();
